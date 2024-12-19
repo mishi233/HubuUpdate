80b452bf70095fac45b11a1c0a04b6e319f5bb19
mishi233 <romeromichael9753@gmail.com>
2024-12-19 07:35:22 -0500

    Fix deploy

--- src/app/pages/principal/steps/review/review.component.ts
+++ src/app/pages/principal/steps/review/review.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [],
+  imports: [ MatTableModule, CommonModule ],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
 export class ReviewComponent {
+  dataSource = [
+    {
+      prioridad: 'Alta',
+      procedimiento: 'Rehidratación intravenosa, suplementación de vitaminas y minerales, dieta balanceada.',
+      costo_final: 250000
+    },
+    {
+      prioridad: 'Media',
+      procedimiento: 'Administración de alimentos especiales, control de peso, monitoreo de salud.',
+      costo_final: 120000
+    },
+    {
+      prioridad: 'Baja',
+      procedimiento: 'Consulta veterinaria y seguimiento, dieta casera recomendada.',
+      costo_final: 80000
+    }
+  ];  
 
+  displayedColumns: string[] = ['prioridad', 'procedimiento', 'costo_final'];
+  
 }
